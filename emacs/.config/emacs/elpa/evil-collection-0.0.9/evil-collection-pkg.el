3351876ad28f3479f1c93388bf8d15d93ac4f577
neo <nyst.wysrt@gmail.com>
2024-01-01 22:13:03 +0800

    update

---
+++ emacs/.config/emacs/elpa/evil-collection-0.0.9/evil-collection-pkg.el
@@ -0,0 +1,16 @@
+(define-package "evil-collection" "0.0.9" "A set of keybindings for Evil mode"
+  '((emacs "26.3")
+    (evil "1.2.13")
+    (annalist "1.0"))
+  :commit "88e5ce8bdb0762119b4153ed3346b486029f26d3" :authors
+  '(("James Nguyen" . "james@jojojames.com"))
+  :maintainers
+  '(("James Nguyen" . "james@jojojames.com"))
+  :maintainer
+  '("James Nguyen" . "james@jojojames.com")
+  :keywords
+  '("evil" "tools")
+  :url "https://github.com/emacs-evil/evil-collection")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,12 +1,79 @@
-;; Dont show the splash screen
-(setq inhibit-startup-message t)
-(setq visible-bell nil)
+;; Dont show splash screen
+(setq-default inhibit-startup-message t)
 
 ;; Turn off some unneeded UI elements
 (menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+
+;; Disable bell notification
+(setq-default visible-bell nil)
+
+;; Initialize package sources
+(require 'package)
+(setq package-archives '(("melpa-stable" . "https://stable.melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+    (package-refresh-contents))
+
+;; Initialize use-package on non linux platforms
+(unless (package-installed-p 'use-package)
+    (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+                treemacs-mode-hook
+                eshell-mode-hook))
+        (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; Keybindings Configuration
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; evil mode
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+  
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+;; Smex
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+;; Enable ido mode
+(ido-mode t)
+
+;; Set fonts
+(set-frame-font "JetBrainsMonoNerdFont 11" nil t)
 
 ;; Display line numbers in every buffer
-(global-display-line-numbers-mode 1)
+(global-display-line-numbers-mode t)
 
 ;; Highlight the current line
 (global-hl-line-mode 1)
@@ -45,11 +112,13 @@
 ;; 'y' for 'yes', 'n' for 'no'
 (fset 'yes-or-no-p 'y-or-n-p)
 
+;; Auto indent after return
+(global-set-key (kbd "RET") 'newline-and-indent)
+
+;; Dired
+(setq dired-listing-switches "-agho --group-directories-first")
+(setq dired-kill-when-opening-new-dired-buffer t)
+
 ;; Theme customization
-(setq modus-themes-mode-line '(accented borderless padded)
-      modus-themes-region '(bg-only)
-      modus-themes-completions '(opiniotated)
-      modus-themes-bold-constructs t
-      modus-themes-paren-match '(bold intense)
-      modus-themes-syntax '(yellow-comments))
-(load-theme 'modus-vivendi t)
+(add-hook 'after-init-hook
+          (lambda () (load-theme 'cyberpunk t)))

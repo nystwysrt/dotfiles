71fe01196911206733de2e09c07d4b788ac52e36
neo <nyst.wysrt@gmail.com>
2024-12-02 15:18:46 +0800

    * Modify emacs config

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,15 +1,20 @@
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (load custom-file 'no-error)
 
+(add-to-list 'default-frame-alist `(font, "IosevkaTerm NFM Regular-14"))
 (setq inhibit-startup-message 1)
+
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (tooltip-mode -1)
 (column-number-mode 1)
+(global-display-line-numbers-mode 1)
+(blink-cursor-mode 1)
 
 ;; Set up the visible bell
-(setq visible-bell t)
+(setq visible-bell 1)
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
